fbe65d4b365db9dda1828e6317f8cb955e773a43
naoya.shimizu <Naoya_Shimizu@jrass.jp>
2024-04-02 17:26:41 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by PC-SYSKAI555 on 2024/04/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
